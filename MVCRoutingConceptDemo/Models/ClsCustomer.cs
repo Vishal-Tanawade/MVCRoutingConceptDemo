2bfc4e843ec1378dda9c0e1c359f137a9f539c2f
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-06-07 19:33:53 +0530

    Poco class added

---
+++ MVCRoutingConceptDemo/Models/ClsCustomer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCRoutingConceptDemo.Models
+{
+    public class ClsCustomer
+    {
+        public int? CustCode { get; set; }
+        public string CustName { get; set; }
+        public DateTime? CustDOJ { get; set; }
+    }
+}
